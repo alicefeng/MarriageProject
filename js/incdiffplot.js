5ecb31bc90d0f462f59977bda5568b719042a3be
Alice Feng <feng.a.g@gmail.com>
2016-06-19 20:23:08 -0400

    Change income difference plot to show stacked bars by occupation

--- js/incdiffplot.js
+++ js/incdiffplot.js
@@ -1,68 +1,98 @@
-var w_diff = 800,
-	h_diff = 400,
-	padding_diff = 50;	
+var w_diff = 900,
+	h_diff = 800,
+	padding_diff = 50,
+	padding_left = 400;	
 
 // set up scales
-var yScale_i = d3.scale.linear()
-	.range([h_diff, 0]);
+var xScale_i = d3.scale.linear()
+	.domain([0, 1])
+	.rangeRound([0, w_diff]);
 
-// set up y axis
+var yScale_i = d3.scale.ordinal()
+	.rangeRoundBands([0, h_diff], .1);
+
+var color_i = d3.scale.ordinal()
+	.range(["#e66101", "#fdb863", "#f7f7f7", "#b2abd2"]);
+
+// set up axes
+var xAxis_i = d3.svg.axis()
+	.scale(xScale_i)
+	.orient("top")
+	.ticks(5, "%");
+
 var yAxis_i = d3.svg.axis()
 	.scale(yScale_i)
-	.orient("right")
-	.ticks(5, "%")
-	.tickSize(w_diff);
+	.orient("left");
 
 // set up chart
 var incPlot = d3.select("#incdiffplot")
   .append("svg")
-	.attr("width", w_diff + padding_diff + padding_diff)
-	.attr("height", h_diff + padding_diff + padding_diff)
+	.attr("width", w_diff + padding_left + padding_diff)
+	.attr("height", h_diff + padding_diff)
   .append("g")
-  	.attr("transform", "translate(" + padding_diff + ", " + padding_diff + ")");
+  	.attr("transform", "translate(" + padding_left + ", " + padding_diff + ")");
 
 
 //load data and draw chart
 d3.csv("data/incdiffdata.csv", function(d) {
 
 	return {
-		label: d.label,
-		value: +d.value,
-		cat: d.cat,
-		order: d.order
+		Occupation: d.Occupation,
+		Rank: +d.Rank,
+		LessHalf: +d.LessHalf,
+		Half: +d.Half,
+		Equal: +d.Equal,
+		HalfMore: +d.HalfMore
 	};
 
 }, function(error, data) {
 
+	color_i.domain(d3.keys(data[0]).filter(function(key) { return key !== "Occupation" && key !== "Rank"; }));
+
+	// reshape data for stacking
+	data.forEach(function(d) {
+		var x0 = 0;
+		d.cat = color_i.domain().map(function(name) { return {name: name, x0: x0, x1: x0 += +d[name]}; });
+		d.total = d.cat[d.cat.length - 1].x1;
+	});
+
 	console.log(data);
 
-	// add in domains for axes
-	yScale_i.domain([0, d3.max(data, function(d) { return d.value; }) ]);
+	// sort data in order of occupation rank
+	data.sort(function(a, b) { return a.Rank - b.Rank; });
 
-	// set bar width
-	var barWidth = w_diff / data.length;
-	console.log(barWidth);
-	
-	// draw axis
-	var incAxis = incPlot.append("g")
+	// add in domains for axes
+	yScale_i.domain(data.map(function(d) { return d.Occupation; }));
+
+
+	// draw axes
+	incPlot.append("g")
+		.attr("class", "axis diff")
+		.attr("transform", "translate(0," + padding_diff/2 + ")")
+		.call(xAxis_i);
+
+	incPlot.append("g")
 		.attr("class", "axis diff")
 		.call(yAxis_i);
 
-	incAxis.selectAll("text")
-		.attr("x", -25)
-		.attr("text-anchor", "end");
 
 	// draw bars
-	var bars = incPlot.selectAll("rect")
+	var bars = incPlot.selectAll(".occ")
 		.data(data)
 		.enter()
+	  .append("g")
+	  	.attr("class", "g")
+	  	.attr("transform", function(d) { return "translate(0," + yScale_i(d.Occupation) + ")"; });
+	
+	bars.selectAll("rect")
+		.data(function(d) { return d.cat; })
+		.enter()
 	  .append("rect")
-	  	.attr("class", "diffbar")
-	  	.attr("width", barWidth - 1)
-	  	.attr("height", 0)
-	  	.attr("x", function(d, i) { return i * barWidth; })
-	  	.attr("y", h_diff);
-
+	  	.attr("x", function(d) { return xScale_i(d.x0); })
+		.attr("width", function(d) { return xScale_i(d.x1) - xScale_i(d.x0); })
+	  	.attr("height", yScale_i.rangeBand())
+	  	.style("fill", function(d) { return color_i(d.name); });
+/*
 	// add in transition
 	incPlot.selectAll("rect")
 		.transition()
@@ -71,6 +101,7 @@ d3.csv("data/incdiffdata.csv", function(d) {
 		.ease("linear")
 		.attr("height", function(d) { return h_diff - yScale_i(d.value); })
 		.attr("y", function(d) { return yScale_i(d.value); });
+
 
 	// add labels
 	incPlot.append("text")
@@ -150,5 +181,5 @@ d3.csv("data/incdiffdata.csv", function(d) {
 		.attr("x", barWidth * 16 + 2)
 		.attr("y", h_diff + 30)
 		.text("than triple wife's");
-
+*/
 }); 
